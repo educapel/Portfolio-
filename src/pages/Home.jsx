bbf4c06d8057acd01b62a9979899281cbe4da94d
EduCapel <educapel@hotmail.com>
2025-12-18 15:11:43 +0100

    stat component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import {ThemeToggle} from "../components/ThemeToggle.jsx";
+import {ThemeToggle} from "../components/ThemeToggle";
+import {StarBackground} from "@/components/StarBackground";
 
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             {/*theme toggle*/}
-            <ThemeToggle/>
+            <ThemeToggle />
 
             {/*background effect*/}
+            <StarBackground />
 
 
             {/*Nav Bar*/}
